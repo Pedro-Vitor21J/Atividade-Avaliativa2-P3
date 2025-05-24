f47db11800dccf9cfdff1b5550f21b2d4bc84930
Pedro Vitor <pedrovtor8@gmail.com>
2025-05-24 16:18:19 -0300

    Feat: Cria projeto

---
+++ android/app/src/main/kotlin/com/github/pedrovitor21j/atividade_avaliativa2_p3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.github.pedrovitor21j.atividade_avaliativa2_p3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/Questionario.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Questionario extends StatefulWidget {
+  const Questionario({super.key});
+
+  @override
+  State<Questionario> createState() => _QuestionarioState();
+}
+
+class _QuestionarioState extends State<Questionario> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
